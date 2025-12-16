e926b5df37879784731e74d31d8af036e034b620
Rada <sirada_dee@cmu.ac.th>
2025-12-16 14:57:13 +0700

    lab8_1.cpp completed

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -1,3 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  char rank;
+  cout << "Input your rank: ";
+  cin >> rank;
+
+    if (rank == 'S'){
+      cout << "You have received Super Ultra Rare Unit!!!\n";
+    }
+    if (rank == 'S' || rank == 'A'){
+      cout << "You have received 5 gems.\n";
+    }
+    if (rank == 'S' || rank == 'A' || rank == 'B'){
+      cout << "You have received 1 gems.\n";
+    }
+    if (rank == 'S' || rank == 'A' || rank == 'B' || rank == 'C'){
+      cout << "You have received 2000 coins.\n";
+    }
+    if (rank == 'S' || rank == 'A' || rank == 'B' || rank == 'C' || rank == 'D'){
+      cout << "You have received very KAK items.\n";
+    }
+}
+
+/*
 #include <iostream>
 using namespace std;
 
@@ -15,3 +42,4 @@ int main()
   }
   return 0;
 }
+*/
